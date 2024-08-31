b79de4eb601e26ac42848a52e8aca98cbe22f7be
nadavgu <nadavgu@gmail.com>
2024-08-31 16:30:48 +0300

    Gradle project

---
+++ agent-plugin/plugin/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    java
+}
+
+group = "org.matilda"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
