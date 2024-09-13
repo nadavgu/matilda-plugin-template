bd66a10b15558c6598fde85a7300b392a6798fea
nadavgu <nadavgu@gmail.com>
2024-09-13 14:55:26 +0300

    Added protobuf

--- agent-plugin/plugin/src/main/java/org/matilda/template/MathService.java
+++ agent-plugin/plugin/src/main/java/org/matilda/template/MathService.java
@@ -2,6 +2,7 @@ package org.matilda.template;
 
 import org.matilda.commands.MatildaCommand;
 import org.matilda.commands.MatildaService;
+import org.matilda.template.protobuf.Exercise;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -54,5 +55,21 @@ public class MathService {
     @MatildaCommand
     public FunctionService createAdder(int amount) {
         return (value) -> value + amount;
+    }
+
+    @MatildaCommand
+    public double solveExercise(Exercise exercise) {
+        switch (exercise.getOperation()) {
+            case ADD:
+                return exercise.getFirstOperand() + exercise.getSecondOperand();
+            case SUBTRACT:
+                return exercise.getFirstOperand() - exercise.getSecondOperand();
+            case MULTIPLY:
+                return exercise.getFirstOperand() * exercise.getSecondOperand();
+            case DIVIDE:
+                return exercise.getFirstOperand() / exercise.getSecondOperand();
+            default:
+                throw new IllegalArgumentException("Unsupported operation: " + exercise.getOperation());
+        }
     }
 }
