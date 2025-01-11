4aaf1542a28d07e67483faa4b211ea74c03f7f60
nadavgu <nadavgu@gmail.com>
2025-01-11 18:50:40 +0200

    Using new format for plugin entry points to support native plugin

--- template/template_plugin.py
+++ template/template_plugin.py
@@ -1,8 +1,14 @@
 from maddie.dependency import Dependency
 from maddie.dependency_container import DependencyContainer
+from matilda.platform.matilda_platform import MatildaPlatform
+from matilda.plugins.plugin_entry_point import PluginEntryPoint
+
 from template.generated.commands.math_service import MathService
 
-PLUGIN_ENTRY_POINT_CLASS_NAME = "org.matilda.template.TemplatePlugin"
+PLUGIN_ENTRY_POINTS = {
+    MatildaPlatform.JVM: PluginEntryPoint("org.matilda.template.TemplatePlugin"),
+    MatildaPlatform.LINUX_X64: PluginEntryPoint("createCommandRegistry"),
+}
 
 
 def load_plugin(dependencies_container: DependencyContainer):
