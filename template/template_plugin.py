c587b7a28d5bdc7eb7632088b05c6a94cb74cfa6
nadavgu <nadavgu@gmail.com>
2025-06-04 02:37:32 +0300

    Using new format for plugin entry points

--- template/template_plugin.py
+++ template/template_plugin.py
@@ -1,8 +1,12 @@
 from maddie.dependency import Dependency
 from maddie.dependency_container import DependencyContainer
 from template.generated.commands.math_service import MathService
+from matilda.platform.matilda_platform import MatildaPlatform
+from matilda.plugins.plugin_entry_point import PluginEntryPoint
 
-PLUGIN_ENTRY_POINT_CLASS_NAME = "org.matilda.template.TemplatePlugin"
+PLUGIN_ENTRY_POINTS = {
+    MatildaPlatform.JVM: PluginEntryPoint("org.matilda.template.TemplatePlugin"),
+}
 
 
 def load_plugin(dependencies_container: DependencyContainer):
